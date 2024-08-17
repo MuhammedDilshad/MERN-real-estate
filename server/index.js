4ba0aa3a57e46a0c943deea7dcceb090e0d51bf6
dilshad <md.mdsar@gmail.com>
2024-08-17 14:32:30 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MNDB = process.env.MONGODB;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MNDB)
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening..");
 });
