0630146bafa2262c95e82229e4e4447a4f2b2d81
dilshad <md.mdsar@gmail.com>
2024-09-19 19:28:37 +0530

    complete update user functionality

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
