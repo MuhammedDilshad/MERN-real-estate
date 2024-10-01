fd0a8cdb4ff6063e9c8b508e586e1275bfcc2d35
dilshad <md.mdsar@gmail.com>
2024-10-01 16:12:17 +0530

    complete user listing

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -2,8 +2,6 @@ import Listing from "../modals/listingModel.js";
 
 export const createListing = async (req, res, next) => {
   try {
-    console.log(req.body, "listing");
-
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
